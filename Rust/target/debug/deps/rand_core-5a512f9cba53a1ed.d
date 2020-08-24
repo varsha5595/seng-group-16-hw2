5d2e964c149cdaf8bfd6ab6b70faecca59df0a0e
shiva96b <shiva96b@gmail.com>
2020-08-24 06:05:45 -0400

    Rust GOL

--- Rust/target/debug/deps/rand_core-5a512f9cba53a1ed.d
+++
@@ -1,7 +0,0 @@
-/Users/shiva/seng-group-16-hw2/Rust/target/debug/deps/rand_core-5a512f9cba53a1ed.rmeta: /Users/shiva/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.3.1/src/lib.rs
-
-/Users/shiva/seng-group-16-hw2/Rust/target/debug/deps/librand_core-5a512f9cba53a1ed.rlib: /Users/shiva/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.3.1/src/lib.rs
-
-/Users/shiva/seng-group-16-hw2/Rust/target/debug/deps/rand_core-5a512f9cba53a1ed.d: /Users/shiva/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.3.1/src/lib.rs
-
-/Users/shiva/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.3.1/src/lib.rs:
--- Rust/target/debug/game_of_life.d
+++
@@ -1,1 +0,0 @@
-/Users/shiva/seng-group-16-hw2/Rust/target/debug/game_of_life: /Users/shiva/seng-group-16-hw2/Rust/src/main.rs
